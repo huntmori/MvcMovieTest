a695ef9cc8322b0b836c9f3a231f15837b3f1aab
HYUN N <kknd5050@hotmail.com>
2018-08-30 21:33:23 +0900

    Initialize Test  Commit

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
-
+//Test Comment for commits
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
